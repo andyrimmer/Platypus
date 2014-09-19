3aca62356e4ee091f5a8e7c026c3c0536fe3a9de
andyrimmer <andyrimmer@gmail.com>
2014-09-19 14:30:04 +0100

    Fix build script to give error messages when build fails.

--- release/buildPlatypus.sh
+++ release/buildPlatypus.sh
@@ -2,10 +2,27 @@
 
 echo 'Building Platypus'
 python setup.py build
+
+if [ $? -ne 0 ]
+then
+    echo ''
+    echo 'Setup failed. Check previous lines for errors'
+    echo ''
+    exit 1
+fi
+
 cp -rf build/lib*/* .
 
-echo ''
-echo ''
-echo 'Finished building Platypus'
-echo ''
-echo ''
+if [ $? -ne 0 ]
+then
+    echo ''
+    echo 'Could not copy shared libraries into current directory'
+    echo ''
+    exit 1
+else
+    echo ''
+    echo ''
+    echo 'Finished building Platypus'
+    echo ''
+    echo ''
+fi
