andyrimmer/Platypus

commit 23398cd6ac093b55df092e460f62a4c28b457d8b

Read tail trimming is now correct for forward/reverse reads

src/cython/cwindow.pyx
@@ -334,7 +334,6 @@ cdef int checkAndTrimRead(cAlignedRead* theRead, cAlignedRead* theLastRead, int 
     true if read is ok, and false otherwise.
     """
     if Read_IsSecondaryAlignment(theRead):
-        #logger.warning("Argh. Found secondary alignment")
         Read_SetQCFail(theRead)
         return False
 
@@ -408,37 +407,23 @@ cdef int checkAndTrimRead(cAlignedRead* theRead, cAlignedRead* theLastRead, int 
     ## Any read that gets passed here will be used, but low quality tails will be trimmed, and
     ## any overlapping pairs, from small fragments, will have the overlapping bits trimmed.
 
-    # Trim low-quality tails
-    for index from 1 <= index <= theRead.rlen:
+    # Trim low-quality tails for forward reads
+    if not Read_IsReverse(theRead):
 
-        # Always trim up to 'minFlank' bases
-        if index <= trimReadFlank:
-            theRead.qual[theRead.rlen - index] = 0
+        for index from 1 <= index <= theRead.rlen:
+            if index <= trimReadFlank or theRead.qual[theRead.rlen - index] < 5:
+                theRead.qual[theRead.rlen - index] = 0
+            else:
+                break
 
-        # Otherwise, trim if qual is below 'minBaseQual'
-        #elif theRead.qual[theRead.rlen - index] < minBaseQual:
-        if theRead.qual[theRead.rlen - index] < 5:
-            theRead.qual[theRead.rlen - index] = 0
+    # Trim low-quality tails for reverse reads
+    else:
+        for index from 0 <= index < theRead.rlen:
 
-        else:
-            #continue
-            break
-
-    # Trim low-quality heads
-    for index from 0 <= index < theRead.rlen:
-
-        # Always trim up to 'minFlank' bases
-        #if index < minFlank:
-        #    theRead.qual[index] = 0
-
-        # Otherwise, trim if qual is below 'minBaseQual'
-        #elif theRead.qual[index] < minBaseQual:
-        if theRead.qual[index] < 5:
-            theRead.qual[index] = 0
-
-        else:
-            #continue
-            break
+            if index <= trimReadFlank or theRead.qual[index] < 5:
+                theRead.qual[index] = 0
+            else:
+                break
 
     cdef int absIns = abs(theRead.insertSize)
 

commit 99fd2371689b93720cc0e88e2e81ffe038234d88

Fixed bug in read trimming
Fixed a bug introduced in 23398cd that caused an additional base
quality to be zeroed on each read flank (in addition to ‘trimReadFlank’
option). In particular this caused every read flank to be zeroed if
‘trimReadFlank’ was left to default - leading to potential under-calls.

src/cython/cwindow.pyx
@@ -407,25 +407,24 @@ cdef int checkAndTrimRead(cAlignedRead* theRead, cAlignedRead* theLastRead, int 
 
     ## Any read that gets passed here will be used, but low quality tails will be trimmed, and
     ## any overlapping pairs, from small fragments, will have the overlapping bits trimmed.
-
+     
     # Trim low-quality tails for forward reads
     if not Read_IsReverse(theRead):
 
         for index from 1 <= index <= theRead.rlen:
-            if index <= trimReadFlank or theRead.qual[theRead.rlen - index] < 5:
+            if index < trimReadFlank or theRead.qual[theRead.rlen - index] < 5:
                 theRead.qual[theRead.rlen - index] = 0
             else:
                 break
-
     # Trim low-quality tails for reverse reads
     else:
         for index from 0 <= index < theRead.rlen:
 
-            if index <= trimReadFlank or theRead.qual[index] < 5:
+            if index < trimReadFlank or theRead.qual[index] < 5:
                 theRead.qual[index] = 0
             else:
                 break
-
+    
     cdef int absIns = abs(theRead.insertSize)
 
     # Trim overlapping part of forward read, in pairs where the read length is greater than the insert size
@@ -435,7 +434,7 @@ cdef int checkAndTrimRead(cAlignedRead* theRead, cAlignedRead* theLastRead, int 
     if trimOverlapping == 1 and (Read_IsPaired(theRead) and absIns > 0 and (not Read_IsReverse(theRead)) and Read_MateIsReverse(theRead) and absIns < 2*theRead.rlen):
         for index from 1 <= index < min(theRead.rlen, (2*theRead.rlen - theRead.insertSize) + 1):
             theRead.qual[theRead.rlen - index] = 0
-
+    
     # Trim the end of any read where the insert size is < read length. If these have not been
     # already filtered out then they need trimming, as adapter contamination will cause a
     # high FP rate otherwise.
@@ -569,7 +568,7 @@ cdef class bamReadBuffer(object):
                 logger.debug("Found null read")
             return
         else:
-
+            
             # TODO: Check that this works for duplicates when first read goes into bad reads pile...
             if self.lastRead != NULL:
                 readOk = checkAndTrimRead(theRead, self.lastRead, self.minGoodBases, self.filteredReadCountsByType, self.minMapQual, self.minBaseQual, self.minFlank, self.trimOverlapping, self.trimAdapter, self.trimReadFlank)
@@ -578,7 +577,7 @@ cdef class bamReadBuffer(object):
                     self.isSorted = False
             else:
                 readOk = checkAndTrimRead(theRead, NULL, self.minGoodBases, self.filteredReadCountsByType, self.minMapQual, self.minBaseQual, self.minFlank, self.trimOverlapping, self.trimAdapter, self.trimReadFlank)
-
+            
             self.lastRead = theRead
 
             # Put read into bad array, and remove from good array

src/cython/variant.pyx
@@ -554,7 +554,7 @@ cdef class VariantCandidateGenerator(object):
         cdef int misMatchEndRef = -1
         cdef int misMatchStartRead = -1
         cdef int misMatchEndRead = -1
-
+        
         for index from 0 <= index < lenSeqToCheck:
 
             # Don't look at the first 'minFlank' bases for candidate generation
@@ -564,7 +564,7 @@ cdef class VariantCandidateGenerator(object):
             # Don't look at the last 'minFlank' bases for candidate generation
             if index + readOffset >= read.rlen - minFlank:
                 continue
-
+            
             #if index + readOffset + refOffset >= read.rlen:
             #    logger.info("read len = %s. index = %s. read offset = %s. ref offset = %s" %(read.rlen, index, readOffset, refOffset))
 
@@ -578,9 +578,7 @@ cdef class VariantCandidateGenerator(object):
             assert baseQual <= 93, "Something is very wrong. Base qual is %s" %(baseQual)
 
             if readChar != refChar:
-
                 if readChar != 'N' and refChar != 'N' and baseQual >= self.minBaseQual:
-
                     if misMatchStartRef == -1:
                         misMatchStartRef = refIndex
                         misMatchEndRef = refIndex
@@ -650,7 +648,7 @@ cdef class VariantCandidateGenerator(object):
         cdef char* readSeq = read.seq
         cdef bytes insertedSequence = None
         cdef bytes deletedSequence = None
-
+        
         for cigarIndex from 0 <= cigarIndex < cigarLength:
 
             flag = read.cigarOps[2*cigarIndex]
@@ -746,7 +744,6 @@ cdef class VariantCandidateGenerator(object):
         cdef int compressed = 0
 
         while readStart != readEnd:
-
             compressed = 0
 
             if Read_IsQCFail(readStart[0]):

src/cython/variantcaller.pyx
@@ -378,13 +378,13 @@ cdef list generateVariantsInRegion(bytes chrom, int start, int end, bamFiles, Fa
 
         # We want to check all BAMs and accumulate data before filtering
         for theReadBuffer in readBuffers:
-
+            
             # Make generator per sample
             varCandGen = VariantCandidateGenerator((chrom,start,end), refFile, options.minMapQual, options.minFlank, options.minBaseQual, options.maxReads, maxReadLength, options, options.verbosity, options.genSNPs, options.genIndels)
 
             if theReadBuffer.reads.getLengthOfLongestRead() > longestRead:
                 longestRead = theReadBuffer.reads.getLengthOfLongestRead()
-
+            
             varCandGen.addCandidatesFromReads(theReadBuffer.reads.array, theReadBuffer.reads.array + theReadBuffer.reads.getSize())
             #varCandGen.addCandidatesFromReads(theReadBuffer.badReads.array, theReadBuffer.badReads.array + theReadBuffer.badReads.getSize())
             if options.verbosity >= 3:
@@ -484,7 +484,7 @@ cdef void callVariantsInRegion(bytes chrom, int start, int end, bamFiles, FastaF
 
     try:
         readBuffers = loadBAMData(bamFiles, chrom, start, end, options, samples, samplesByID, samplesByBAM, refSequence)
-
+    
     except Exception, e:
         logger.error('Exception in region %s:%s-%s. Error was %s' %(chrom, start, end, e))
         logger.warning("Region %s:%s-%s will be skipped" %(chrom, start, end))